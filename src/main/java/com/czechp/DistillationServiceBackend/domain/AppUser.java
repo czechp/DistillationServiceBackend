6aac1a7f4b423fd53ccc96429f8a4d565d82fcb1
PCzech <you@example.com>
2021-01-07 20:28:01 +0100

    Added validation dependency

---
+++ src/main/java/com/czechp/DistillationServiceBackend/domain/AppUser.java
@@ -0,0 +1,58 @@
+package com.czechp.DistillationServiceBackend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity(name = "users")
+@AllArgsConstructor()
+@NoArgsConstructor()
+@Data()
+public class AppUser implements UserDetails {
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
