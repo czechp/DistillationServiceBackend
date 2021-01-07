6555e273f6cbda4ecc2c4162e9e8925c3e9e7b06
PCzech <you@example.com>
2021-01-07 20:20:54 +0100

    Initial commit

---
+++ src/main/java/com/czechp/DistillationServiceBackend/DistillationServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.czechp.DistillationServiceBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistillationServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistillationServiceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czechp/DistillationServiceBackend/DistillationServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czechp.DistillationServiceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistillationServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
